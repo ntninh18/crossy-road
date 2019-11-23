5928096b97d3117cb4694c509946850159c8c16f
ntninh18 <ntninh18@apcs.vn>
2019-11-23 17:23:16 +0700

    text

--- CrossyRoad/CrossyRoad/CGraphics.cpp
+++ CrossyRoad/CrossyRoad/CGraphics.cpp
@@ -27,5 +27,13 @@ void Text(int x, int y) {
 	gotoXY(x, y + 5); cout << draw(2) << space(5) << draw(2) << space(4) << draw(3) << space(2) << draw(3) << space(4) << draw(5) << space(4) << draw(5) << space(4) << draw(5) << endl;
 	gotoXY(x, y + 6); cout << draw(3) << space(4) << draw(2) << space(4) << draw(3) << space(2) << draw(3) << space(1) << draw(1) << space(4) << draw(3) << space(1) << draw(1) << space(4) << draw(3) << space(4) << draw(5) << endl;
 	gotoXY(x, y + 7); cout << draw(6) << space(1) << draw(2) << space(5) << draw(7) << space(2) << draw(7) << space(2) << draw(7) << space(4) << draw(5) << endl;
-	gotoXY(x, y + 8); cout << space(1) << draw(6) << space(1) << draw(2) << space(5) << draw(7) << space(2) << draw(7) << space(2) << draw(7) << space(4) << draw(5) << endl;
+	gotoXY(x, y + 8); cout << space(1) << draw(5) << space(1) << draw(2) << space(6) << draw(5) << space(3) << draw(6) << space(3) << draw(6) << space(6) << draw(3) << endl;
+	gotoXY(x, y + 9); cout << space(44) << draw(3) << endl;
+	gotoXY(x, y + 10); cout << space(41) << draw(5) << endl;
+	gotoXY(x, y + 11); cout << space(9) << draw(5) << space(2) << draw(5) << space(4) << draw(5) << space(2) << draw(6) << space(3) << draw(5) << endl;
+	gotoXY(x, y + 12); cout << space(9) << draw(5) << space(1) << draw(7) << space(2) << draw(7) << space(1) << draw(7) << space(2) << draw(2) << endl;
+	gotoXY(x, y + 13); cout << space(9) << draw(4) << space(1) << draw(3) << space(2) << draw(3) << space(6) << draw(3) << space(1) << draw(3) << space(2) << draw(3) << endl;
+	gotoXY(x, y + 14); cout << space(9) << draw(2) << space(3) << draw(3) << space(2) << draw(3) << space(6) << draw(3) << space(1) << draw(3) << space(3) << draw(2) << endl;
+	gotoXY(x, y + 15); cout << space(9) << draw(2) << space(3) << draw(3) << space(2) << draw(3) << space(2) << draw(7) << space(1) << draw(3) << space(3) << draw(2) << endl;
+	gotoXY(x, y + 16); cout << space(9) << draw(2) << space(3) << draw(3) << space(2);
 }
--- CrossyRoad/CrossyRoad/Main.cpp
+++ CrossyRoad/CrossyRoad/Main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
 	FixConsoleWindow();
-	resizeConsole(950, 500);
-	Text(5, 5);
+	resizeConsole(1300, 700);
+	Text(30, 5);
 	//CCAR car;
 	//for (int i = 5; i < 50; i++) {
 	//	car.Move(i, 20);
